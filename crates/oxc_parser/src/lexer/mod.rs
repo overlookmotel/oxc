dfd98102d940f4691b4046dd715107733ca6a634
overlookmotel <theoverlookmotel@gmail.com>
2024-01-23 20:47:33 +0000

    Remove duplicate code

--- crates/oxc_parser/src/lexer/mod.rs
+++ crates/oxc_parser/src/lexer/mod.rs
@@ -479,15 +479,6 @@ impl<'a> Lexer<'a> {
                 bytes.next();
                 continue;
             }
-
-            if b == b'\\' {
-                #[cold]
-                fn backslash<'a>(lexer: &mut Lexer<'a>, bytes: BytesIter<'a>) -> &'a str {
-                    &lexer.identifier_after_backslash(bytes, false)[1..]
-                }
-                return backslash(self, bytes);
-            }
-
             if b == b'\\' {
                 #[cold]
                 fn backslash<'a>(lexer: &mut Lexer<'a>, bytes: BytesIter<'a>) -> &'a str {
