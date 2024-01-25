c8b1e7de02433f6b1d0fbf0b31e4e983d97a69f4
overlookmotel <theoverlookmotel@gmail.com>
2024-01-25 07:53:26 +0000

    SIMD v9

--- crates/oxc_parser/src/lexer/identifier.rs
+++ crates/oxc_parser/src/lexer/identifier.rs
@@ -167,11 +167,11 @@ impl<'a> Lexer<'a> {
                 fn is_identifier_part_ascii_byte(b: u8) -> u8 {
                     u8::from(
                         // Weirdly, the order of these conditions makes a massive impact
-                        b == b'_'
-                            || b == b'$'
-                            || b.wrapping_sub(b'A') < 26
-                            || b.wrapping_sub(b'a') < 26
-                            || b.wrapping_sub(b'0') < 10,
+                        (b == b'_')
+                            | (b == b'$')
+                            | (b.wrapping_sub(b'A') < 26)
+                            | (b.wrapping_sub(b'a') < 26)
+                            | (b.wrapping_sub(b'0') < 10),
                     ) * 0xFF
                 }
                 // SAFETY: We know for sure slice is at least `SIMD_LANES` bytes long
