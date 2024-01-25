573c4a8017e80569c2502916787c470523dc3495
overlookmotel <theoverlookmotel@gmail.com>
2024-01-25 03:36:55 +0000

    Comment

--- crates/oxc_parser/src/lexer/identifier.rs
+++ crates/oxc_parser/src/lexer/identifier.rs
@@ -71,7 +71,7 @@ impl<'a> Lexer<'a> {
         // End of identifier found (which may be EOF).
         // Advance `self.current.chars` up to after end of identifier.
         // `bytes` must be positioned on a UTF-8 character boundary, as we've only consumed ASCII
-        // bytes from it, so converting `bytes` to a `&str` is safe.
+        // bytes from it, so converting `bytes` to `Chars` is safe.
         self.current.chars = bytes.chars_unchecked();
 
         // Return identifier minus its first char.
